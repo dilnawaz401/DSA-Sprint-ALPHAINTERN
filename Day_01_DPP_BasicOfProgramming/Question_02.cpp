778a3071b1c5a87d208896cb7f1e6f2915bd3e56
dilnawaz401 <dilnawazansari401@gmail.com>
2025-11-30 18:54:37 +0530

    Remove unused files and update output subproject commits

--- Day_01_DPP_BasicOfProgramming/Question_02.cpp
+++
@@ -1,9 +0,0 @@
-//// Tprint "Hello, World!" to the console
-#include <iostream>
-using namespace std;
-int main() 
-{
-    // Print "Hello, World!" to the console
-    cout << "Hello, World!" << endl; 
-    return 0; // Indicate that the program ended successfully
-}
